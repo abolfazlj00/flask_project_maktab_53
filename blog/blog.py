a91f78494848a822321c69db14c643f952efcbaa
sepehrab1996 <sepehrab1996@gmail.com>
2021-08-13 04:26:24 +0430

    add index view to blog.py

--- blog/blog.py
+++ blog/blog.py
@@ -3,9 +3,25 @@ from blog.db import get_db
 
 bp = Blueprint("blog", __name__)
 
-@bp.route("/home/")
-def home():
-    return "hello home"
+
+@bp.route('/', defaults={'page': None})
+@bp.route('/<page>/')
+def index(page):
+    if page is None:
+        return render_template('base.html')
+    else:
+        if page not in ('home', 'login', 'register', 'profile'):
+            return "Page not Found!", 404
+        content_html = f"{page}_content.html"
+        if page in ('login', 'register'):
+            return render_template(f'auth/{content_html}')
+        else:
+            return render_template(content_html)
+
+
+# @bp.route("/home/")
+# def home():
+#     return "hello home"
 
 
 @bp.route("/post/<int:post_id>/")
@@ -22,6 +38,6 @@ def category(category_id):
 def tag(tag_id):
     return f'tag_id is {tag_id}'
 
-@bp.route("/login/")
-def login():
-    return 'yourwelcome to our page please login or register'
+# @bp.route("/login/")
+# def login():
+#     return 'yourwelcome to our page please login or register'

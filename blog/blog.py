7285a532911e2f03e097f9ae3148f2112c2f8eb5
sepehrab1996 <sepehrab1996@gmail.com>
2021-08-13 20:54:06 +0430

    blog.py changed

--- blog/blog.py
+++ blog/blog.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect
+from flask import Blueprint, render_template, request, redirect, session, url_for
 from werkzeug.utils import secure_filename
 import hashlib
 import base64
@@ -36,7 +36,7 @@ def register():
         hashed_password = hashlib.sha512((password + salt).encode()).hexdigest()
 
         email = request.form.get('email')
-        # phone_number = request.form.get('phone')
+        phone_number = request.form.get('phone')
         f = request.files.get('image')
         if f:
             file_name = secure_filename(f.filename)
@@ -48,38 +48,51 @@ def register():
     db = get_db()
     user = {'f_name': f_name, 'l_name': l_name, 'password': hashed_password, 'salt': salt, 'username': username,
             'email': email,
-            'image': image}
-    skip = 0
+            'image': image, 'phone_number': phone_number}
+    skip_username = 0
+    skip_phone = 0
     for i in range(db.users.count()):
-        if skip == 0:
+        if skip_username == 0 and skip_phone == 0:
             if username == db.users.find({}, {'username': 1, "_id": 0})[i]["username"]:
-                skip = 1
-
-    if skip == 0:
+                skip_username = 1
+            if phone_number == db.users.find({}, {'phone_number': 1, "_id": 0})[i]["phone_number"]:
+                skip_phone = 1
+    if skip_username == 0 and skip_phone == 0:
         db.users.insert_one(user)
         return {'f_name': f_name, 'l_name': l_name}
 
     else:
-        return "ERROR"
+        if skip_username == 1:
+            return "این نام کاربری قبلا ثبت شده است"
+        return "شماره همراه قبلا ثبت شده است"
 
 
 @bp.route("/login", methods=["POST"])
 def login():
     db = get_db()
-    users = db.items.find()
     username = request.form.get('username')
     password = request.form.get('password')
-    login_user = db.users.find({"username": username}, {"_id":0})
+    login_user = db.users.find({"username": username}, {"_id": 0})
 
     if login_user.count() != 0:
         valid_password = login_user[0]["password"]
         generated_password = hashlib.sha512((password + login_user[0]["salt"]).encode()).hexdigest()
         if valid_password == generated_password:
-            return login_user[0]
+            session["username"] = username
+            return {'f_name': login_user[0]["f_name"], 'l_name': login_user[0]["l_name"],
+                    "username": session["username"]}
+
         else:
-            return "wrong password"
+            return "رمز عبور اشتباه است"
     else:
-        return "User not found"
+        return "نام کاربری پیدا نشد"
+
+
+@bp.route("/logout", methods=["GET"])
+def logout():
+    session.pop('username', None)
+    print(session)
+    return ""
 
 
 @bp.route("/post/<int:post_id>/")

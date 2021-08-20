c557a85df6f446259f89e9eb6c805b8cb19f439c
morteza078 <mortezarobatjazi.77@gmail.com>
2021-08-20 10:25:18 -0700

    change on buttons_for_my_post.js

--- blog/static/js/buttons_for_my_post.js
+++ blog/static/js/buttons_for_my_post.js
@@ -6,26 +6,46 @@ function post_state(state, post_id) {
         success: function () {
             if (state === 1) {
                 alert('پست شما با موفقیت فعال شد')
+                view_home()
             } else {
                 alert('پست شما با موفقیت غیرفعال شد')
             }
-            $('#close-modal').click()
+             $('.modal-backdrop').remove();
+            view_home()
         }
     })
 }
 
-function drop_post(post_id) {
+function delete_post(data) {
+    if (confirm('آیا می خواهید این پست را حذف کنید؟')) {
+  // Save it
+        console.log(data.id)
+         $.ajax({
+        type: "POST",
+        data: data.id,
+        url: "/user/delete_post",
+        success: function (data) {
+            $('.modal-backdrop').remove();
+           view_home()
+        }
+    })
+
+} else {
+  // Do nothing!
+}
+
+}
+
+
+function edit_post(post_id) {
+
     $.ajax({
         type: 'POST',
-        url: '/user/drop-post/',
+        url: '/user/edit_post/',
         data: {'post_id': post_id},
-        success: function () {
-
-            alert('پست شما با موفقیت حذف گردید')
-            $('#close-modal').click()
-
+        success: function (data) {
+             $('.content').html(data)
+             $('.modal-backdrop').remove();
         }
     })
 }
-
-

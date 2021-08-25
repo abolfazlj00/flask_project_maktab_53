2560da475c927189ec67e9577a8053f98ec2408a
sepehrab1996 <sepehrab1996@gmail.com>
2021-08-25 10:14:55 +0430

    some bug fixes (modal and scroll)

--- blog/static/js/select_tag.js
+++ blog/static/js/select_tag.js
@@ -2,12 +2,13 @@ function select_tag_function(data) {
     console.log(data.id)
     $.ajax({
         type: "POST",
-        data: {"tag":data.id},
+        data: {"tag": data.id},
         url: "/user/posts-list-by-tag/",
         success: function (data) {
-            console.log(data)
-               $(".content").html(data)
-             $('.modal-backdrop').remove()
+            $(".content").html(data)
+            $('.modal-backdrop').remove()
+            $('#body').css("overflow-y", "scroll")
+            $('#body').css("padding-right", "0")
         }
     })
-    }
+}

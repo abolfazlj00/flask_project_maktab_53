0725bebaaa1b639ae29e6763ca1c4289901518a6
morteza078 <mortezarobatjazi.77@gmail.com>
2021-08-27 19:29:43 -0700

    debugging search.js file

--- blog/static/js/search.js
+++ blog/static/js/search.js
@@ -1,22 +1,30 @@
-$('#serach-form').submit(function (e) {
-    e.preventDefault(); // avoid to execute the actual submit of the form.
+var searched_data
+function serach(searched_data) {
+    $('#serach-form').submit(function (e) {
+        e.preventDefault(); // avoid to execute the actual submit of the form.
 
-    var form = $(this);
-    var url = form.attr('action');
-    var formData = new FormData(this);
-    $.ajax({
-        type: "POST",
-        url: url,
-        data: formData, // serializes the form's elements.
-        success: function (response) {
-            $('.content').html(response)
-        },
-        cache: false,
-        contentType: false,
-        processData: false
+        var form = $(this);
+        var url = form.attr('action');
+        var formData = new FormData(this);
+        searched_data=formData.search
+        var select = document.getElementById("type_of_search");
+        var value_of_select = select.value;
+        formData.append("type_of_search", value_of_select)
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: formData, // serializes the form's elements.
+            success: function (response) {
+                $('.content').html(response)
+            },
+            cache: false,
+            contentType: false,
+            processData: false
+        });
     });
-});
 
-$('#submit-search').click(function (){
-    $('#serach-form').submit()
-})
+    $('#submit-search').click(function () {
+        $('#serach-form').submit()
+    })
+}
+serach(searched_data)

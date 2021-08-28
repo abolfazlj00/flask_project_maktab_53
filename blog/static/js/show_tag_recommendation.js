0065511bb5b640f5469925daf0c21423a5ff2dfd
morteza078 <mortezarobatjazi.77@gmail.com>
2021-08-27 19:32:33 -0700

    change on show_tag_recommendation.js for performance

--- blog/static/js/show_tag_recommendation.js
+++ blog/static/js/show_tag_recommendation.js
@@ -5,8 +5,8 @@ function show_tag_recommendation() {
         success: function (data) {
             var list_of_all_tags = data["list_of_all_tags"]
             for (let i in list_of_all_tags) {
-                $("#for_tag_rec").append(`<a href="#" id=${list_of_all_tags[i]} onclick="select_tag_function(this)"
-                                       class="badge badge-primary p-2 m-2" style="font-size: large"># ${list_of_all_tags[i]}</a>`)
+                $("#for_tag_rec").append(`<span  id=${list_of_all_tags[i]} 
+                                       class="badge badge-pill badge-primary p-3 m-2" style="font-size: large"># ${list_of_all_tags[i]}</span>`)
             }
         }
     })

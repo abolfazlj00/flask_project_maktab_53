c3bf6d7de01a754f98c4f984bed8d29e5847906b
sepehrab1996 <sepehrab1996@gmail.com>
2021-08-28 15:19:38 +0430

    changing some sytle of alert , button , ...

--- blog/static/js/show_tag_recommendation.js
+++ blog/static/js/show_tag_recommendation.js
@@ -6,7 +6,7 @@ function show_tag_recommendation() {
             var list_of_all_tags = data["list_of_all_tags"]
             for (let i in list_of_all_tags) {
                 $("#for_tag_rec").append(`<span  id=${list_of_all_tags[i]} 
-                                       class="badge badge-pill badge-primary p-3 m-2" style="font-size: large"># ${list_of_all_tags[i]}</span>`)
+                                       class="badge badge-pill p-3 m-2 " style="font-size: large; background-color: darkslategray; color: white"># ${list_of_all_tags[i]}</span>`)
             }
         }
     })
